0b71eb32d8e992adf6e496aa18b9fb9ace519a96
J4ngel <andres_angel@live.com.ar>
2022-11-21 19:26:04 -0500

    init push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Imagen } from './models/imagen';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba_tecnica_2';
+  fileName = '';
+  filepath:any;
+  constructor(private http: HttpClient) {}
+
+  imgs_array: Imagen[] = [
+    {id:0,src:"https://wallpaperaccess.com/full/3971879.jpg"},
+    {id:1,src:"https://wallpaperaccess.com/full/1347921.jpg"},
+    {id:2,src:"https://wallpaperaccess.com/full/3971885.jpg"},
+  ];
+  new_img:Imagen = new Imagen;
+  ind:string = "0";
+
+  onFileSelected(event:any) {
+
+    const file:File = event.target.files[0];
+
+    if (file) {
+        this.fileName = file.name;
+        let reader = new FileReader();
+        reader.onload = (event:any) => {
+        this.filepath = event.target.result;
+        console.log(this.filepath)
+        }
+        console.log(reader.readAsDataURL(file))
+    }
+}
+
+  addImage(){
+    this.imgs_array.push(this.new_img)
+  }
+
+  delImage(){
+    this.imgs_array.pop()
+  }
+
 }
